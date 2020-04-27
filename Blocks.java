4cf19590e1a7a07774810098ab3a511eaef0b7fa
Krish Srivatsav <krish@cornhub.gov>
2020-04-27 16:54:45 +0530

    first commit

---
+++ Blocks.java
@@ -0,0 +1,11 @@
+package Demo;
+
+public class Blocks {
+
+	
+public void ValidateHeader()
+{
+	System.out.println("Header links validated");
+}
+
+}
---
+++ CallBlock.java
@@ -0,0 +1,16 @@
+package Demo;
+
+public class CallBlock {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("I Navigated to home page");
+		
+		Blocks b=new Blocks(); //new is a memory allocation operator creates class for memory..
+		b.ValidateHeader();
+		//syntax is classobject.methodname
+		
+		
+	}
+
+}
